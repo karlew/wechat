622e43986e64d285d41087b40ea7af8b52031443
karlew <lu@karlew.com>
2016-03-16 23:58:55 +0800

    edit

--- menu/deleteMenu.php
+++ menu/deleteMenu.php
@@ -5,29 +5,28 @@
 
 header('Content-Type: text/html; charset=UTF-8');
 
-$APPID="wx9b4b97f6ce6647ef";
-$APPSECRET="b1be97744f78714a5de17f4ca2823201";
+//替换为自己的appid和secret
+$appid="wx9b4b97f6ce6647ef";
+$secret="b1be97744f78714a5de17f4ca2823201";
 
-$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
+//获取access_token
+$token_url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$appid."&secret=".$secret;
+$json=file_get_contents($token_url);
+$res=json_decode($json);
+$access_token=$res->access_token;
 
-$json=file_get_contents($TOKEN_URL);
-$result=json_decode($json);
+//GET方式请求
+$menu_url="https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=".$access_token;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $menu_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$msg = json_decode(curl_exec($ch),true);
+curl_close($ch);
 
-$ACC_TOKEN=$result->access_token;
-
-$MENU_URL="https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=".$ACC_TOKEN;
-
-$cu = curl_init();
-curl_setopt($cu, CURLOPT_URL, $MENU_URL);
-curl_setopt($cu, CURLOPT_RETURNTRANSFER, 1);
-$info = curl_exec($cu);
-$res = json_decode($info);
-curl_close($cu);
-
-if($res->errcode == "0"){
-	echo "菜单删除成功";
+if($msg['errcode'] == "0"){
+	echo "删除成功";
 }else{
-	echo "菜单删除失败";
+	echo "删除失败";
 }
 
 ?>
